58d9b2c6443cbd6468b9bd8daa71190923483207
denversimmons <35702708+denversimmons@users.noreply.github.com>
2018-11-05 19:26:55 -0800

    Update WAVRead.cpp

--- WAVRead.cpp
+++ WAVRead.cpp
@@ -3,12 +3,6 @@
 #include <iostream>
 #include <stdint.h>
 #pragma warning(disable:4996)
-#define tableSize 100000
-
-struct uint24
-{
-	unsigned int  int24 : 24;
-};
 
 float* readWAV(const char fileName[])
 {
@@ -22,8 +16,15 @@ float* readWAV(const char fileName[])
 	}
 
 	char type[4];
-	uint32_t size, chunk1Size, WAVsampleRate, avgBytesPerSec, chunk2Size;
-	short formatType, channels, bytesPerSample, bitsPerSample;
+	uint32_t fileSize;		//Size of file in bytes - 8
+	uint32_t chunk1Size;		//Size of format chunk in bytes
+	uint32_t WAVsampleRate;		//Samplerate of file
+	uint32_t avgBytesPerSec;	//Number of frames per second (samples per second in ALL CHANNELS)
+	uint32_t chunk2Size;		//Size of data chunk in bytes
+	short formatType;		//Format type. This program will only support 1 and 3
+	short channels;			//Number of channels
+	short blockAlign;		//Number of bytes in a frame (bytes per sample in ALL CHANNELS)			
+	short bitsPerSample;		//Bit depth. This program will only support 8, 16, and 32 bit depth
 
 	fread(type, sizeof(char), 4, fp);
 	if (!strcmp(type, "RIFF"))
@@ -32,8 +33,7 @@ float* readWAV(const char fileName[])
 		return nullptr;
 	}
 
-	fread(&size, sizeof(uint32_t), 1, fp);
-	size -= 8;
+	fread(&fileSize, sizeof(uint32_t), 1, fp);
 	fread(type, sizeof(char), 4, fp);
 	if (!strcmp(type, "WAVE"))
 	{
@@ -53,7 +53,7 @@ float* readWAV(const char fileName[])
 	fread(&channels, sizeof(short), 1, fp);
 	fread(&WAVsampleRate, sizeof(uint32_t), 1, fp);
 	fread(&avgBytesPerSec, sizeof(uint32_t), 1, fp);
-	fread(&bytesPerSample, sizeof(short), 1, fp);
+	fread(&blockAlign, sizeof(short), 1, fp);
 	fread(&bitsPerSample, sizeof(short), 1, fp);
 	fread(type, sizeof(char), 4, fp);
 	if (!strcmp(type, "data"))
@@ -64,18 +64,17 @@ float* readWAV(const char fileName[])
 
 	fread(&chunk2Size, sizeof(uint32_t), 1, fp);
 
-	int sample_size = bitsPerSample / 8;
-	int samples_count = (chunk2Size * 8) / (channels * bitsPerSample);
+	int bytesPerSample = bitsPerSample / 8;
+	int samples_count = (chunk2Size) / (blockAlign);
 	
 	std::cout << "Chunk 1 Size: " << chunk1Size << std::endl;
 	std::cout << "Format Type: " << formatType << std::endl;
 	std::cout << "Channels: " << channels << std::endl;
 	std::cout << "Sample Rate: " << WAVsampleRate << std::endl;
 	std::cout << "Byte Rate: " << avgBytesPerSec << std::endl;
-	std::cout << "Bytes Per Sample: " << bytesPerSample << std::endl;
+	std::cout << "Block Align: " << blockAlign << std::endl;
 	std::cout << "Bits Per Sample: " << bitsPerSample << std::endl;
 	std::cout << "Chunk 2 Size: " << chunk2Size << std::endl;
-	std::cout << "Sample Size: " << sample_size << std::endl;
 	std::cout << "Samples Count: " << samples_count << std::endl;
 
 	// Begin Parsing
@@ -84,7 +83,6 @@ float* readWAV(const char fileName[])
 	float fSamp;
 	uint8_t iSamp8;
 	int16_t iSamp16;
-	uint24 iSamp24;
 	int32_t iSamp32;
 
 	if (formatType == 1)
@@ -93,33 +91,26 @@ float* readWAV(const char fileName[])
 		{
 			if (bitsPerSample == 8)
 			{
-				fread(&iSamp8, sample_size, 1, fp);
+				fread(&iSamp8, bytesPerSample, 1, fp);
 				fSamp = ((float)iSamp8 - 127.5f) / (float)127.5;
 				if (fSamp > 1) fSamp = 1;					
 				if (fSamp < -1) fSamp = -1;
 			}
 			if (bitsPerSample == 16)
 			{
-				fread(&iSamp16, sample_size, 1, fp);
+				fread(&iSamp16, bytesPerSample, 1, fp);
 				fSamp = ((float)iSamp16) / (float)32768;					
-				if (fSamp > 1) fSamp = 1;
-				if (fSamp < -1) fSamp = -1;
-			}
-			if (bitsPerSample == 24)
-			{
-				fread(&iSamp24, sample_size, 1, fp);
-			//	fSamp = ((float)iSamp24) / (float)2147483648;
 				if (fSamp > 1) fSamp = 1;
 				if (fSamp < -1) fSamp = -1;
 			}
 			if (bitsPerSample == 32)
 			{
-				fread(&iSamp32, sample_size, 1, fp);
+				fread(&iSamp32, bytesPerSample, 1, fp);
 				fSamp = ((float)iSamp32) / (float)2147483648;
 				if (fSamp > 1) fSamp = 1;
 				if (fSamp < -1) fSamp = -1;
 			}
-			//table[index] = fSamp;
+			table[index] = fSamp;
 		}
 	}
 
@@ -129,18 +120,18 @@ float* readWAV(const char fileName[])
 		{
 			for (int index = 0; index < samples_count; index++)
 			{
-				fread(&table[index], sample_size, 1, fp);
+				fread(&table[index], bytesPerSample, 1, fp);
 			}
 		}
 		if (channels == 2)
 		{
 			for (int index = 0; index < (samples_count / 2); index++)
 			{
-				fread(&table[index], sample_size, 1, fp);
+				fread(&table[index], bytesPerSample, 1, fp);
 			}
 			for (int index = (samples_count / 2); index < samples_count; index++)
 			{
-				fread(&table[index], sample_size, 1, fp);
+				fread(&table[index], bytesPerSample, 1, fp);
 			}
 		}
 	}
